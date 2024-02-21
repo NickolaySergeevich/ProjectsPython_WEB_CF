75aa84bd8e8e4b4d9cee534f142696b001275e28
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-02-21 12:46:53 +0300

    Добавил функцию для обработки запроса (шаблон).

--- calculator_three/views.py
+++ calculator_three/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from calculator_three.models import CalculatorHistory
+
+
+def index_page(request):
+    context = {"page_name": "Калькулятор"}
+
+    if request.method == "POST":
+        number_one = int(request.POST["number_one"])
+        number_two = int(request.POST["number_two"])
+
+        answer = number_one + number_two
+
+        CalculatorHistory(
+            number_one=number_one, number_two=number_two, answer=answer
+        ).save()
+
+    return render(request, "calculator_three/index_page.html", context)
