55ecad555b01d70e4f170179169f3a560a1e40df
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-02-23 22:01:00 +0300

    Дописал проект.

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from notes.models import Notes, NotesSupplements
+
+
+def index_page(request):
+    context = {"page_name": "Notes"}
+
+    if request.method == "POST":
+        note_header = request.POST.get("note_header")
+        note_description = request.POST.get("note_description")
+
+        note_item = Notes(note_header=note_header, note_description=note_description)
+        note_item.save()
+
+        note_supplements = (
+            request.POST.get("note_supplement_one", default=None),
+            request.POST.get("note_supplement_two", default=None),
+            request.POST.get("note_supplement_three", default=None),
+            request.POST.get("note_supplement_four", default=None),
+            request.POST.get("note_supplement_five", default=None),
+        )
+        for note_supplement in note_supplements:
+            if note_supplement:
+                NotesSupplements(
+                    note_id=note_item,
+                    note_supplement=note_supplement,
+                ).save()
+
+    data = Notes.objects.all().values()
+    for index in range(len(data)):
+        data[index]["note_supplements"] = NotesSupplements.objects.filter(
+            note_id=data[index]["id"]
+        ).values()
+
+    context["data"] = data
+
+    return render(request, "notes/index_page.html", context)
