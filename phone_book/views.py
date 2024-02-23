7c35822e30fc97567b5e4593ade8391773f0e707
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-02-23 10:37:08 +0300

    Проверил показ страницы. Сделал заготовку для функции-обработчика.

--- phone_book/views.py
+++ phone_book/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def index_page(request):
-    pass
+    context = {"page_name": "Телефонная книга"}
+
+    if request.method == "POST":
+        pass
+    elif request.method == "GET":
+        pass
+
+    return render(request, "phone_book/index_page.html", context)
