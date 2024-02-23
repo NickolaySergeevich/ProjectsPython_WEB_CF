64456ff1490605d64872900593ce4a0a2b550e4d
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-02-23 10:42:33 +0300

    Поменял тип поля для телефонного номера.

--- phone_book/models.py
+++ phone_book/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class PhoneBook(models.Model):
     contact_name = models.TextField()
     contact_surname = models.TextField()
-    contact_phone = models.IntegerField()
+    contact_phone = models.TextField()
     contact_notes = models.TextField()
