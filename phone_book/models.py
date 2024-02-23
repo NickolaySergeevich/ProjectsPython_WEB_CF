466ba8a97ca00a7b9421972d2b81880983476cf3
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-02-23 10:40:17 +0300

    Добавил модель для телефонной книги.

--- phone_book/models.py
+++ phone_book/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class PhoneBook(models.Model):
+    contact_name = models.TextField()
+    contact_surname = models.TextField()
+    contact_phone = models.IntegerField()
+    contact_notes = models.TextField()
