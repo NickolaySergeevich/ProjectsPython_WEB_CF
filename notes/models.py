505bed8d074790716da9df0b96291f0837da27f4
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-02-23 21:36:01 +0300

    Добавил модели для работы с бд.

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Notes(models.Model):
+    note_header = models.TextField()
+    note_description = models.TextField()
+
+
+class NotesSupplements(models.Model):
+    note_id = models.ForeignKey(Notes, on_delete=models.CASCADE)
+    note_supplement = models.TextField()
